14949b396dd2e5b4d9fd86461c4f792ecc0d6854
Daffa Andika O <192800590+Dapaaw@users.noreply.github.com>
2025-09-10 21:09:44 +0700

    sudah

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Row and Column',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Program Pertamaku"),
-        ),
-        body: const Center(
-          child: Text(
-            "Daffa Andika Octadaniswara",
-            style: TextStyle(fontSize: 24),
+        appBar: AppBar(title: Text('Row and Column')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  KotakBiruJempol(color: Colors.blue),
+                  SizedBox(width: 16),
+                  KotakBiruJempol(color: Colors.red),
+                  SizedBox(width: 16),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  KotakBiruJempol(color: Colors.green),
+                  SizedBox(width: 16),
+                  KotakBiruJempol(color: Colors.orange),
+                  SizedBox(width: 16),
+                ],
+              ),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class KotakBiruJempol extends StatelessWidget {
+  final Color color;
+  
+  KotakBiruJempol({this.color = Colors.blue});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: color,
+        border: Border.all(color: Colors.black, width: 2),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Icon(Icons.favorite, color: Colors.white, size: 40),
     );
   }
 }
